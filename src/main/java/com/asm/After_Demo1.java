16a7bde89f1c8bc0f087069f9cb8a8ac70ea5ae9
zhaichen <z@123>
2015-09-02 13:07:49 +0800

    local-------------------------------------

--- src/main/java/com/asm/After_Demo1.java
+++ src/main/java/com/asm/After_Demo1.java
@@ -4,6 +4,5 @@ public class After_Demo1 {
 	public void sayHello(){
 		System.out.println("before");
 		System.out.println("hello");
-		System.out.println("after");
 	}
 }
--- src/main/java/com/new1/NewClass.java
+++ src/main/java/com/new1/NewClass.java
@@ -3,5 +3,6 @@ package com.new1;
 public class NewClass {
 	public static void main(String[] args) {
 		System.out.println("<-----------------local change--------------------->");
+		int a;
 	}
 }
