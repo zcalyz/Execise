e04f10c1b20dca89f41cd5de617b8843375b2c44
zcalyz <21451123@zju.edu.cn>
2015-09-02 13:49:59 +0800

    remote

--- src/main/java/com/new1/NewClass.java
+++ src/main/java/com/new1/NewClass.java
@@ -4,5 +4,6 @@ public class NewClass {
 	public static void main(String[] args) {
 		System.out.println("<-----------------local change--------------------->");
 		System.out.println("###########################################");
+		System.out.println("remote");
 	}
 }
